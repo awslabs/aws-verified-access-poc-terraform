c0cd504365219ca5217d248372df9e65cc626d12
Andy Van Becelaere <avanbece@amazon.com>
2024-07-10 19:48:04 -0600

    minor updates & iam policy improvements

--- app_resources.tf
+++ app_resources.tf
@@ -2,19 +2,34 @@
 # SPDX-License-Identifier: MIT-0
 
 module "vpc" {
-  source = "./modules/vpc"
+  #checkov:skip=CKV_TF_1:Commit hash not required
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~>5.9.0"
+
   providers = {
     aws = aws.app
   }
 
-  env                = var.environment
-  name               = var.app_name
-  azs                = var.vpc_config.azs
-  cidr               = var.vpc_config.cidr[0]
-  private_subnets    = var.vpc_config.private_subnets
-  public_subnets     = var.vpc_config.public_subnets
-  enable_nat_gateway = true
-  single_nat_gateway = false
+  name = "${var.app_name}-${var.environment}"
+  cidr = var.vpc_config.cidr[0]
+
+  azs                                = var.vpc_config.azs
+  private_subnets                    = var.vpc_config.private_subnets
+  public_subnets                     = var.vpc_config.public_subnets
+  create_database_subnet_group       = false
+  create_database_subnet_route_table = false
+  manage_default_network_acl         = true
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Environment = var.environment
+  }
 }
 
 module "verified_access_sg" {
@@ -55,7 +70,7 @@ module "ecs" {
 
   app_name         = var.app_name
   vpc_id           = module.vpc.vpc_id
-  vpc_cidr         = module.vpc.vpc_cidr
+  vpc_cidr         = module.vpc.vpc_cidr_block
   private_subnets  = module.vpc.private_subnets
   public_subnets   = module.vpc.public_subnets
   environment_list = []
